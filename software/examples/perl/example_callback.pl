2e42acfdc1a79d36479aaa898a0b39cfd9a5c7a6
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 16:04:48 +0100

    Add Perl examples

---
+++ software/examples/perl/example_callback.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickStepper;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => '63oL6P'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $stepper = BrickStepper->new(&UID, $ipcon); # Create device object
+
+sub cb_reached
+{
+    my ($position) = @_;
+
+    my $steps = 0;
+    my $vel = 0;
+    my $dec = 0;
+
+    if(int(rand(1)))
+    {
+        $steps = int(rand(5000 - 1000)) + 1000; # steps (forward)
+        print"\nDriving forward: ".$steps." steps\n";
+    }
+    else
+    {
+        $steps = int(rand(5000 - 1000)) + 1000; # steps (backward)
+        print"\nDriving backward: ".$steps." steps\n";
+
+    $vel = int(rand(2000 - 200)) + 200; # steps/s
+    $acc = int(rand(1000 - 100)) + 100; # steps/s^2
+    $dec = int(rand(1000 - 100)) + 100; # steps/s^2
+    print"\nConfiguration (vel, acc, dec): ".$vel.", ".$acc.", ".$dec."\n";
+
+    $stepper->set_speed_ramping($acc, $dec);
+    $stepper->set_max_velocity($vel);
+    $stepper->set_steps($steps);
+    }
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Register "position reached callback" to cb_reached
+# cb_reached will be called every time a position set with
+# set_steps or set_target_position is reached
+$stepper->register_callback($stepper->CALLBACK_POSITION_REACHED, 'cb_reached');
+
+$stepper->enable();
+$stepper->set_steps(1); # Drive one step forward to get things going
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
---
+++ software/examples/perl/example_configuration.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickStepper;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => '63oL6P'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $stepper = BrickStepper->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+$stepper->set_motor_current(800); # 800mA
+$stepper->set_step_mode(8); # 1/8 step mode
+$stepper->set_max_velocity(6000); # Velocity 2000 steps/s
+
+# Slow acceleration (500 steps/s^2), 
+# Fast deacceleration (5000 steps/s^2)
+$stepper->set_speed_ramping(500, 5000); 
+
+$stepper->enable();
+$stepper->set_steps(60000); # Drive 60000 steps forward
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
