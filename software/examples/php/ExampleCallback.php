0cc95e3781c302f6541115f7d04c0e3dab106e9e
Matthias Bolte <matthias@tinkerforge.com>
2014-01-15 11:37:04 +0100

    php: Use const for constant values

--- software/examples/php/ExampleCallback.php
+++ software/examples/php/ExampleCallback.php
@@ -6,9 +6,9 @@ require_once('Tinkerforge/BrickStepper.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickStepper;
 
-$host = 'localhost';
-$port = 4223;
-$uid = '9eggGYnDtnd'; // Change to your UID
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = '9eggGYnDtnd'; // Change to your UID
 
 // Use position reached callback to program random movement
 function cb_reached($position, $userData)
@@ -34,9 +34,9 @@ function cb_reached($position, $userData)
 }
 
 $ipcon = new IPConnection(); // Create IP connection
-$stepper = new BrickStepper($uid, $ipcon); // Create device object
+$stepper = new BrickStepper(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
 // Register "position reached callback" to cb_reached
--- software/examples/php/ExampleConfiguration.php
+++ software/examples/php/ExampleConfiguration.php
@@ -6,14 +6,14 @@ require_once('Tinkerforge/BrickStepper.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickStepper;
 
-$host = 'localhost';
-$port = 4223;
-$uid = '9eggGYnDtnd'; // Change to your UID
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = '9eggGYnDtnd'; // Change to your UID
 
 $ipcon = new IPConnection(); // Create IP connection
-$stepper = new BrickStepper($uid, $ipcon); // Create device object
+$stepper = new BrickStepper(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
 $stepper->setMotorCurrent(800); // 800mA
