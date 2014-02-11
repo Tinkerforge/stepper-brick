046e76e8dec9e79de30bdd184c488a7fc350a393
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 15:30:15 +0100

    javascript: short examples

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -10,9 +10,7 @@ var stepper = new BrickStepper(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
-            console.log('Error: Already connected');        
-        }
+        console.log('Error: '+error);        
     }
 );// Connect to brickd
 
@@ -49,14 +47,10 @@ stepper.on(BrickStepper.CALLBACK_POSITION_REACHED,
 );
 
 console.log("Press any key to exit ...");
-process.stdin.on('data', function(data) {
-	    ipcon.disconnect(
-            function(error) {
-                if(error === IPConnection.ERROR_NOT_CONNECTED) {
-                    console.log('Error: Not connected');        
-                }
-            }
-        );
-process.exit(0);
-});
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
 
--- software/examples/javascript/ExampleConfiguration.js
+++ software/examples/javascript/ExampleConfiguration.js
@@ -10,9 +10,7 @@ var stepper = new BrickStepper(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
-            console.log('Error: Already connected');        
-        }
+        console.log('Error: '+error);        
     }
 );// Connect to brickd
 
@@ -31,14 +29,10 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
 );
 
 console.log("Press any key to exit ...");
-process.stdin.on('data', function(data) {
-	    ipcon.disconnect(
-            function(error) {
-                if(error === IPConnection.ERROR_NOT_CONNECTED) {
-                    console.log('Error: Not connected');        
-                }
-            }
-        );
-process.exit(0);
-});
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
 
