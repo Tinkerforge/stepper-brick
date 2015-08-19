56a35b8230ac6aba9041960d3c42f09090e79e4c
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 10:23:23 +0200

    Cleanup Perl examples

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickStepper;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '63oL6P'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 our $stepper = Tinkerforge::BrickStepper->new(&UID, $ipcon); # Create device object
--- software/examples/perl/example_configuration.pl
+++ software/examples/perl/example_configuration.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickStepper;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '63oL6P'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $stepper = Tinkerforge::BrickStepper->new(&UID, $ipcon); # Create device object
@@ -15,15 +15,16 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 
 $stepper->set_motor_current(800); # 800mA
 $stepper->set_step_mode(8); # 1/8 step mode
-$stepper->set_max_velocity(6000); # Velocity 2000 steps/s
+$stepper->set_max_velocity(2000); # Velocity 2000 steps/s
 
-# Slow acceleration (500 steps/s^2), 
+# Slow acceleration (500 steps/s^2),
 # Fast deacceleration (5000 steps/s^2)
-$stepper->set_speed_ramping(500, 5000); 
+$stepper->set_speed_ramping(500, 5000);
 
 $stepper->enable();
 $stepper->set_steps(60000); # Drive 60000 steps forward
 
 print "Press any key to exit...\n";
 <STDIN>;
+$stepper->disable();
 $ipcon->disconnect();
