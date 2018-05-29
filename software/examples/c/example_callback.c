f341213fc601719768e2c7d50301f0a37e47574e
Matthias Bolte <matthias@tinkerforge.com>
2018-05-29 20:13:44 +0200

    Minor example tweaks

--- software/examples/c/example_callback.c
+++ software/examples/c/example_callback.c
@@ -10,10 +10,10 @@
 
 // Use position reached callback to program random movement
 void cb_position_reached(int32_t position, void *user_data) {
+	(void)position; // avoid unused parameter warning
+
 	Stepper *stepper = (Stepper *)user_data;
 	int32_t steps;
-
-	(void)position; // avoid unused parameter warning
 
 	if(rand() % 2) {
 		steps = (rand() % 4000) + 1000; // steps (forward)
