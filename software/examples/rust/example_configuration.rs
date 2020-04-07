ec486b42043861359c9702624c0defb2a6038796
Erik Fleckstein <erik@tinkerforge.com>
2020-04-07 15:33:29 +0200

    examples: rust: Always block when calling bindings functions.

--- software/examples/rust/example_configuration.rs
+++ software/examples/rust/example_configuration.rs
@@ -12,26 +12,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    stepper.set_motor_current(800); // 800 mA
-    stepper.set_step_mode(8); // 1/8 step mode
-    stepper.set_max_velocity(2000); // Velocity 2000 steps/s
+    stepper.set_motor_current(800).recv()?; // 800 mA
+    stepper.set_step_mode(8).recv()?; // 1/8 step mode
+    stepper.set_max_velocity(2000).recv()?; // Velocity 2000 steps/s
 
     // Slow acceleration (500 steps/s^2),
     // Fast deacceleration (5000 steps/s^2)
-    stepper.set_speed_ramping(500, 5000);
+    stepper.set_speed_ramping(500, 5000).recv()?;
 
-    stepper.enable(); // Enable motor power
-    stepper.set_steps(60000); // Drive 60000 steps forward
+    stepper.enable().recv()?; // Enable motor power
+    stepper.set_steps(60000).recv()?; // Drive 60000 steps forward
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
 
     // Stop motor before disabling motor power
-    stepper.stop(); // Request motor stop
-    stepper.set_speed_ramping(500, 5000); // Fast deacceleration (5000 steps/s^2) for stopping
+    stepper.stop().recv()?; // Request motor stop
+    stepper.set_speed_ramping(500, 5000).recv()?; // Fast deacceleration (5000 steps/s^2) for stopping
     thread::sleep(Duration::from_millis(400)); // Wait for motor to actually stop: max velocity (2000 steps/s) / decceleration (5000 steps/s^2) = 0.4 s
-    stepper.disable(); // Disable motor power
+    stepper.disable().recv()?; // Disable motor power
 
     ipcon.disconnect();
     Ok(())
